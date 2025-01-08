97e26c3f4c6f1e95fc805b90c032db1bb5d2edb5
Tyler Jung <tylerjung@berkeley.edu>
2025-01-07 20:49:39 -0500

    1/7/2025

--- slides.js
+++ slides.js
@@ -1,8 +1,13 @@
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4){
-        counter = 1;
-    }
-}, 4000);
+function cycleButtons(word) {
+    var counter = 1;
+    setInterval(function() {
+        document.getElementById('radio' + counter + word).checked = true;
+        counter++;
+        if (counter > 4) {
+            counter = 1;
+        }
+    }, 5000);
+}
+
+cycleButtons(""); 
+cycleButtons("-mag");
