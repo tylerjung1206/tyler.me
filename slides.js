4c612548dde012b5765efa46902397cab9371d76
Tyler Jung <tylerjung@berkeley.edu>
2025-01-08 23:11:43 -0500

    more changes

--- slides.js
+++ slides.js
@@ -11,3 +11,17 @@ function cycleButtons(word) {
 
 cycleButtons(""); 
 cycleButtons("-mag");
+
+
+function cycleBbuttons(word) {
+    var counter = 1;
+    setInterval(function() {
+        document.getElementById('radio' + counter + word).checked = true;
+        counter++;
+        if (counter > 2) {
+            counter = 1;
+        }
+    }, 5000);
+}
+
+cycleBbuttons("-worm")
